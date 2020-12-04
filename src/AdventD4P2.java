6ed69bc0ccdbfec044b90083571f5bd82d7b7074
Andrew Sizemore <asizemore456@gmail.com>
2020-12-04 17:04:02 -0500

    Day 4

--- src/AdventD4P2.java
+++ src/AdventD4P2.java
@@ -81,7 +81,7 @@ public class AdventD4P2 {
                 }
             }
         }
-        if (validChecks == 7) {
+        if (validChecks == 1) {
             return true;
         } else {
             return false;
